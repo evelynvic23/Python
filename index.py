9e78a6f7be5eda256c81429b56e7bd834669831d
Evelyn Victoria da Silva Santos <115710253+evelynvic23@users.noreply.github.com>
2023-10-06 09:53:26 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,29 @@
+print('Hello World!')
+
+var1 = int(input('Digite uma nota: '))
+
+var2 = int(input('Digite outra nota: '))
+
+valor1 = (var1 + var2)
+
+if valor1 >= 7:
+    print('Passou de ano! com a nota: ', valor1)
+else:
+    print('Reprovou de ano! com a nota: ', valor1)
+
+
+
+# /////////////////////////////////////////////////////////////////
+
+
+print('CALCULADORA EVELYNZINHA')
+
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+
+soma = numero1 + numero2
+
+print('A soma dos números é: ', soma)
+
+
+
