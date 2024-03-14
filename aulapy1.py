1e8c631c128a2deab608b40a12a5dddc7c4d431d
Evelyn Victoria da Silva Santos <115710253+evelynvic23@users.noreply.github.com>
2024-03-14 08:43:42 -0300

    Add files via upload

---
+++ aulapy1.py
@@ -0,0 +1,61 @@
+# nota1 = float(input("Digite a primeira nota: "))
+# nota2 = float(input("Digite a segunda nota: "))
+ 
+# div = (nota1 + nota2) / 2
+ 
+# print("Calcular média dos alunos")
+ 
+# print("O valor da divisão das notas é: ", div)
+ 
+# # Saída sem conversão de tipo de dado
+ 
+# # print("media = {}".format(div))
+ 
+# # Mostra se foi aprovado ou não
+ 
+ 
+# if div >= 6:
+#     print('Passou de ano querido')
+# # elif é o senão da estrutura de decisão
+# elif div >= 3:
+#      print('recuperação')
+# else:
+#     print('Não passou de ano querido')
+ 
+# # para juntar variáveis com o texto utilizar a vírgula
+
+# letra = str(input("Digite F ou M: "))
+ 
+# if letra == "F" or letra == "f":
+#     print('Feminino')
+# # elif é o senão da estrutura de decisão
+# elif letra == "M" or letra == "m":
+#      print('Masculino')
+# else:
+#     print('Digite apenas F ou M')
+ 
+
+
+
+
+#  print("Calcular Índice de massa corporal")
+ 
+# altura = float(input("Digite sua altura: "))
+ 
+# peso = float(input("Digite seu peso: "))
+ 
+# calculo = peso/(altura * altura)
+ 
+ 
+# if calculo < 18.5:
+#     print("baixo peso")
+# elif calculo < 24.9:
+#     print("peso normal")
+# elif calculo < 29.9:
+#     print("sobrepeso")
+# elif calculo < 34.9:
+#     print("obesidade tipo I")
+# elif calculo < 39.9:
+#     print("obesidade tipo I")
+# else:
+#     print("Obesidade classe III")
---
+++ aulapy2.py
@@ -0,0 +1,40 @@
+# letra = str(input("Digite F ou M: "))
+ 
+# if letra == "F" or letra == "f":
+#     print('Feminino')
+# # elif é o senão da estrutura de decisão
+# elif letra == "M" or letra == "m":
+#      print('Masculino')
+# else:
+#     print('Digite apenas F ou M')
+
+
+# //////////////////////////////////////////////////////////////////////
+
+
+
+# Horas e minutos para apenas minutos
+
+# H = int(input("Digite a hora: "))
+# M = int(input("Digite os minutos: "))
+
+# MT = (H*60)+M
+
+# print("Total de minutos:", MT)
+
+# if MT % 2 == 0:
+#     print("Par:", MT)
+# else:
+#      print("Ìmpar:", MT)
+
+
+# //////////////////////////////////////////////////////////////////////
+
+
+# celsius para fahrenheit
+
+C = float(input("Digite a temperatura em celsius: "))
+
+F = (C * 1.8) + 32
+
+print("A temperatura em Fahrenheit é: ", F)
